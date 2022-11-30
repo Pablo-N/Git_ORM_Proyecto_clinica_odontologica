aaf8dc2f0dfe3093443f346c04f23abc0972e672
Pablo N <Pablo N>
2022-11-29 19:38:17 -0500

    Initialyzr

---
+++ SistemaGestionTurnos/src/main/java/com/PabloNovoa/SistemaGestionTurnos/SistemaGestionTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.PabloNovoa.SistemaGestionTurnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionTurnosApplication.class, args);
+	}
+
+}
---
+++ SistemaGestionTurnos/src/test/java/com/PabloNovoa/SistemaGestionTurnos/SistemaGestionTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PabloNovoa.SistemaGestionTurnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
